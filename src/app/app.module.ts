1c448042d820a06cea9503c7cbf659b5a9f1c881
Jon Ander Aguinaco <jonaguiper@gmail.com>
2019-09-21 21:27:02 +0200

    creando herencia de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CampoBasicoComponent } from './formulario/campo-basico/campo-basico.component';
+import { ElementoFormularioComponent } from './formulario/elemento-formulario/elemento-formulario.component';
+import { FormularioComponent } from './formulario/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampoBasicoComponent,
+    ElementoFormularioComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
