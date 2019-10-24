d217d8186e091a9b60d952d1ef5609b8ec280dd0
Jon Ander Aguinaco <jonaguiper@gmail.com>
2019-10-24 07:34:18 +0200

    implementacion de condiciones en validaciones

--- src/app/servicios/validadores.service.spec.ts
+++ src/app/servicios/validadores.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { ValidadoresService } from './validadores.service';
 import { Validacion } from '../modelos/validacion';
 import { Validators, ValidatorFn, FormControl } from '@angular/forms';
+import { CondicionalesService } from './condicionales.service';
 
 describe('ValidadoresService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -13,31 +14,30 @@ describe('ValidadoresService', () => {
   });
   it('podria ser un null el parametro', () => {
     const service: ValidadoresService = TestBed.get(ValidadoresService);
-    let listaValidatorFn = service.crearValidaciones(null);
+    const listaValidatorFn = service.crearValidaciones(null);
     expect(listaValidatorFn).toBeTruthy();
     expect(listaValidatorFn.length).toEqual(0);
   });
 
   it('podria ser Validator.email', () => {
-    let options = { tipoValidacion: 'email', parametros: ['param1'] };
+    const options = { tipoValidacion: 'email', parametros: ['param1'] };
     const validacion: Validacion = options;
-    let listaValidaciones: Validacion[] = [];
+    const listaValidaciones: Validacion[] = [];
     listaValidaciones.push(validacion);
     const service: ValidadoresService = TestBed.get(ValidadoresService);
-    let listaValidatorFn = service.crearValidaciones(listaValidaciones);
+    const listaValidatorFn = service.crearValidaciones(listaValidaciones);
     expect(listaValidatorFn.length).toBeGreaterThan(0);
     expect(listaValidatorFn[0]).toBeDefined();
   });
 
   it('podria crear lista 2 elementos', () => {
-    let listaValidaciones: Validacion[] = [];
+    const listaValidaciones: Validacion[] = [];
     const validacion: Validacion = { tipoValidacion: 'required', parametros: ['param1'] };
     const validacion2: Validacion = { tipoValidacion: 'email', parametros: ['param2'] };
     listaValidaciones.push(validacion);
     listaValidaciones.push(validacion2);
     const service: ValidadoresService = TestBed.get(ValidadoresService);
-    let listaValidatorFn = service.crearValidaciones(listaValidaciones);
-    debugger;
+    const listaValidatorFn = service.crearValidaciones(listaValidaciones);
     expect(listaValidatorFn.length).toBeGreaterThan(1);
     expect(listaValidatorFn[0]).toBeTruthy();
     expect(listaValidatorFn[1]).toBeTruthy();
@@ -52,40 +52,75 @@ describe('ValidadoresService', () => {
       ]
     };
     const service: ValidadoresService = TestBed.get(ValidadoresService);
-    let listaValidaciones = service.crearListaValidacionesFromJson(listajson);
+    const listaValidaciones = service.crearListaValidacionesFromJson(listajson);
     expect(listaValidaciones).toBeTruthy();
     expect(listaValidaciones.length).toEqual(3);
     expect(listaValidaciones[1].tipoValidacion).toBe('email');
     expect(listaValidaciones[0].tipoValidacion).toBe('required');
   });
-
-
 });
 
-
-describe('Test Funciones de validacion', () => {
+fdescribe('Test de validacion requerido', () => {
   let servicioValidacion: ValidadoresService;
+  let servicioCondiciones: CondicionalesService;
   let formControl: FormControl;
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({});
     servicioValidacion = TestBed.get(ValidadoresService);
     formControl = new FormControl('input');
+    servicioCondiciones = TestBed.get(CondicionalesService);
   });
 
-  it('podria ser un campo requerido invalido', () => {
-    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator();
+  it('podria ser un campo requerido invalido, sin Condiciones', () => {
+    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator(null);
     formControl.setValue('');
     formControl.markAsTouched({ onlySelf: true });
     const respuestaValidacion = validarRequerido(formControl);
     expect(respuestaValidacion).toBeTruthy();
     expect(respuestaValidacion.kcRequiredValidator.mensaje).toBeDefined();
   });
-  it('podria ser un campo requerido valido', () => {
-    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator();
+  it('podria ser un campo requerido valido, sin Condiciones', () => {
+    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator(null);
     expect(validarRequerido(formControl)).toBeNull();
   });
-  it('podria ser un Email invalido correo con ñ', () => {
-    const validarEmail: ValidatorFn = servicioValidacion.kcEmailValidator();
+
+  it('podria ser un campo requerido valido, con Condiciones a true', () => {
+    spyOn(servicioCondiciones, 'evaluar').and.returnValue(true);
+    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator(null);
+    expect(validarRequerido(formControl)).toBeNull();
+  });
+  it('podria ser un campo requerido invalido, con Condiciones a true', () => {
+    spyOn(servicioCondiciones, 'evaluar').and.returnValue(true);
+    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator(null);
+    formControl.setValue('');
+    formControl.markAsTouched({ onlySelf: true });
+    const respuestaValidacion = validarRequerido(formControl);
+    expect(respuestaValidacion).toBeTruthy();
+    expect(respuestaValidacion.kcRequiredValidator.mensaje).toBeDefined();
+  });
+
+  it('podria ser un campo requerido invalido, con Condiciones a false lo que lo no debe ser evaluado', () => {
+    spyOn(servicioCondiciones, 'evaluar').and.returnValue(false);
+    const validarRequerido: ValidatorFn = servicioValidacion.kcRequiredValidator(null);
+    formControl.setValue('');
+    formControl.markAsTouched({ onlySelf: true });
+    const respuestaValidacion = validarRequerido(formControl);
+    expect(respuestaValidacion).toBeNull();
+  });
+});
+
+
+describe('Test de validacion de Email', () => {
+  let servicioValidacion: ValidadoresService;
+  let formControl: FormControl;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    servicioValidacion = TestBed.get(ValidadoresService);
+    formControl = new FormControl('input');
+  });
+
+  it('podria ser un Email invalido correo con ñ, sin Condiciones', () => {
+    const validarEmail: ValidatorFn = servicioValidacion.kcEmailValidator(null);
     formControl.setValue('correoconÑ@invalido.si');
     formControl.markAsDirty({ onlySelf: true });
     formControl.markAsTouched({ onlySelf: true });
@@ -93,13 +128,13 @@ describe('Test Funciones de validacion', () => {
     expect(respuestaValidacion).toBeTruthy();
     expect(respuestaValidacion.kcEmailValidator.mensaje).toBeDefined();
   });
-  it('podria ser un Email valido campo vacio', () => {
-    const validarEmail: ValidatorFn = servicioValidacion.kcEmailValidator();
+  it('podria ser un Email valido campo vacio, sin Condiciones', () => {
+    const validarEmail: ValidatorFn = servicioValidacion.kcEmailValidator(null);
     formControl.setValue('');
     expect(validarEmail(formControl)).toBeNull();
   });
-  it('podria ser un Email valido', () => {
-    const validarEmail: ValidatorFn = servicioValidacion.kcEmailValidator();
+  it('podria ser un Email valido, sin Condiciones', () => {
+    const validarEmail: ValidatorFn = servicioValidacion.kcEmailValidator(null);
     formControl.setValue('correo@valido.si');
     formControl.markAsTouched({ onlySelf: true });
     expect(validarEmail(formControl)).toBeNull();
