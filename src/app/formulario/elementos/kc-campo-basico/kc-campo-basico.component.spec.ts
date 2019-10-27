88078e14d14665716a7c21dbf915a57c086c0723
Jon Ander <jonaguiper@gmail.com>
2019-10-27 07:57:37 +0100

    crear test inicial e inici de test con validaciones

--- src/app/formulario/elementos/kc-campo-basico/kc-campo-basico.component.spec.ts
+++ src/app/formulario/elementos/kc-campo-basico/kc-campo-basico.component.spec.ts
@@ -2,23 +2,30 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KcCampoBasicoComponent } from './kc-campo-basico.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material';
-import { Injector } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { Injector, DebugElement } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataElementoFormularioModel } from 'src/app/modelos/data-elemento-formulario-model';
+import { By } from '@angular/platform-browser';
+import { Validacion } from 'src/app/modelos/validacion';
 
-describe('CampoBasicoComponent', () => {
+fdescribe('KcCampoBasicoComponent', () => {
   let component: KcCampoBasicoComponent;
   let fixture: ComponentFixture<KcCampoBasicoComponent>;
   let testInjector: Injector;
+  let componenteHtml: DebugElement;
 
   beforeEach(() => {
     // refine the test module by declaring the test component
     TestBed.configureTestingModule({
       declarations: [KcCampoBasicoComponent],
       imports: [BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         ReactiveFormsModule,
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatInputModule
       ],
       providers: [Injector]
     });
@@ -27,11 +34,74 @@ describe('CampoBasicoComponent', () => {
     fixture = TestBed.createComponent(KcCampoBasicoComponent);
     // get test component from the fixture
     component = fixture.componentInstance;
-
     testInjector = TestBed.get(Injector);
   });
 
-  it('should create', () => {
+  it('el elemento se crea con todas las propiedades correctamente', () => {
+    const dataElemento = new DataElementoFormularioModel({
+      name: 'inputTest',
+      texto: 'label del elemento',
+      value: 'valor por Defecto'
+    });
+    const formulario = new FormGroup({ [dataElemento.name]: new FormControl(dataElemento.value, []) });
+    spyOn(testInjector, 'get').and.callFake((token) => {
+      switch (token) {
+        case 'texto':
+          return dataElemento.texto;
+        case 'formulario':
+          return formulario;
+        case 'name':
+          return dataElemento.name;
+        case 'value':
+          return dataElemento.value;
+        case 'elementosGrupo':
+          return null;
+      }
+    });
+    fixture.detectChanges();
+    componenteHtml = fixture.debugElement.query(By.css('.mat-form-field'));
+    const input = componenteHtml.query(By.css('#' + dataElemento.name));
+    const label = componenteHtml.query(By.css('label'));
     expect(component).toBeTruthy();
+    expect(input).toBeTruthy();
+    expect(input.nativeElement.value).toEqual(dataElemento.value);
+    expect(label).toBeTruthy();
+    expect(label.nativeElement.innerText).toEqual(dataElemento.texto);
+
+  });
+
+  it('el elemento responde y crea errores correctamente', () => {
+    const validaObligatorio: Validacion[] = [{ tipoValidacion: 'required' }];
+    const dataElemento = new DataElementoFormularioModel({
+      name: 'inputTest',
+      texto: 'label del elemento',
+      value: '',
+      validaciones: validaObligatorio
+    });
+    const formulario = new FormGroup({ [dataElemento.name]: new FormControl(dataElemento.value, []) });
+    spyOn(testInjector, 'get').and.callFake((token) => {
+      switch (token) {
+        case 'texto':
+          return dataElemento.texto;
+        case 'formulario':
+          return formulario;
+        case 'name':
+          return dataElemento.name;
+        case 'value':
+          return dataElemento.value;
+        case 'elementosGrupo':
+          return null;
+      }
+    });
+    fixture.detectChanges();
+    componenteHtml = fixture.debugElement.query(By.css('.mat-form-field'));
+    const input = componenteHtml.query(By.css('#' + dataElemento.name));
+    const label = componenteHtml.query(By.css('label'));
+    expect(component).toBeTruthy();
+    expect(input).toBeTruthy();
+    expect(input.nativeElement.value).toEqual(dataElemento.value);
+    expect(label).toBeTruthy();
+    expect(label.nativeElement.innerText).toEqual(dataElemento.texto);
+
   });
 });
